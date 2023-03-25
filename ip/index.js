b7664ff030cc6b412265e4a378c753c61cac1b54
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-03-26 02:22:11 +0300

    Tamamlandı fakat tam response alınamadı

--- ip/index.js
+++ ip/index.js
@@ -66,3 +66,108 @@ async function ipAdresimiAl() {
 */
 
 //kodlar buraya gelecek
+
+//1
+
+//index.htmle <script src="https://unpkg.com/axios/dist/axios.min.js"></script> tagı eklenince DevToolsda bilgiler geliyor
+
+axios
+  .get("https://apis.ergineer.com/ipgeoapi/88.227.80.20")
+  .then((response) => {
+    return response.data;
+  })
+  .catch((error) => {
+    console.log("başarısız", error);
+  });
+
+//2
+/* 
+
+	"sorgu": 
+    "durum": 
+    "kıta": 
+    "ülke": 
+    "ülkeKodu": 
+    "ülkebayrağı": "https://apis.ergineer.com/ulkebayraklari/TR",
+    "bölge": "06",
+    "bölgeAdı": "Ankara",
+    "şehir": "Ankara",
+    "zip":
+    "enlem": 
+    "boylam": 
+    "saatdilimi": "Europe/Istanbul",
+    "parabirimi": "TRY",
+    "isp": 
+    "organizasyon": 
+    "as": "
+
+	*/
+
+//3
+
+function ipHunter(myData) {
+  //4
+  const cardsDiv = document.getElementsByClassName("cards")[0];
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const image = document.createElement("img");
+  image.src = myData["ülkebayrağı"];
+  const cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+  const cardH3 = document.createElement("h3");
+  cardH3.classList.add("ip");
+  cardH3.textContent = myData.sorgu;
+
+  const cardP = [];
+  for (let i = 0; i <= 5; i++) {
+    const newP = document.createElement("p");
+    cardP.push(newP);
+  }
+  cardP[0].classList.add("ulke");
+  cardP[0].textContent = myData[("ülke", "ülkeKodu")];
+  cardP[1].textContent = `Enlem: ${myData.enlem} Boylam: ${myData.boylam}`;
+  cardP[2].textContent = `Şehir: ${myData["şehir"]}`;
+  cardP[3].textContent = `Saat dilimi:${myData.saatdilimi}`;
+  cardP[4].textContent = `Para birimi:${myData.parabirimi}`;
+  cardP[5].textContent = `ISP:${myData.isp}`;
+
+  p1.textContent = `Enlem: {${data.enlem}} Boylam: {${data.boylam}}`;
+  p2.textContent = `Şehir: {${data.bölgeAdı}}`;
+  p3.textContent = "Saat dilimi: " + data.saatdilimi;
+  p4.textContent = `Para birimi: {${data.parabirimi}}`;
+  p5.textContent = `ISP: {${data.isp}}`;
+
+  cardInfo.appendChild(cardH3);
+  cardInfo.appendChild(cardP);
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+
+  cardsDiv.appendChild(card);
+}
+
+ipHunter(myData);
+/*
+axios
+  .get("https://apis.ergineer.com/ipadresim")
+  .then((response) => {
+    const dinamikIP = response.data;
+    dinamikIP.map(ipHunter);
+  })
+  .catch((error) => {
+    console.log("başarısız", error);
+  });
+*/
+
+async function ipAdresimiAl() {
+  await axios({
+    method: "get",
+    url: "https://apis.ergineer.com/ipadresim",
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (a) {
+      benimIP = a;
+    });
+}
